7b89fd93c0811e296caa00955c9f9eabfb49fe06
King Crawford <crawboy@me.com>
2018-11-22 09:20:56 -0500

    modify timeline views

--- timeline/views.py
+++ timeline/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("This is the timeline index")
+
+def floorplan(request, space):
+    return HttpResponse("This is the floorplan for blah")
+
+def chart(request):
+    return HttpResponse("This is a gantt chart")
