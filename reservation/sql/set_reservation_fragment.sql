c129f45771d40f2a7c163ca9fe25cc6dd0d1d234
King Crawford <crawboy@me.com>
2018-11-27 18:32:50 -0500

    create reservation

---
+++ reservation/sql/set_reservation_fragment.sql
@@ -0,0 +1,5 @@
+SET @customer_app_id = %s, @customer_name = %s, @num_menus = %s, @res_datetime = %s;
+INSERT IGNORE INTO timeline_customer (vip, name, phone, app_id) VALUES (0, @customer_name, '', @customer_app_id);
+SELECT LAST_INSERT_ID() INTO @customer_id;
+INSERT INTO timeline_reservation (num_menus, res_datetime, res_duration, customer_id) VALUES (@num_menus, @res_datetime, 60, @customer_id);
+SELECT LAST_INSERT_ID() INTO @reservations_id;
