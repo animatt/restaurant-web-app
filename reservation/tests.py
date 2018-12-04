ae317d1e94bca08b021f776da0676f113466928e
King Crawford <crawboy@me.com>
2018-12-04 13:12:33 -0500

    requesting a reservation places a hold that must be confirmed

--- reservation/tests.py
+++ reservation/tests.py
@@ -3,22 +3,268 @@ import pprint
 
 from django.utils.html import escape
 from django.utils import timezone
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
-from fake_data import fake_data_generator
+from unittest import mock
 
+from timeline.models import Location, Space, Reservation
+from fake_data import fake_data_generator as fdg
 
-def signin_user(test_obj):
-    user = User.objects.create(username='typical_user', password='open')
+from .views import (
+    hold_seats_for_confirmation,
+    select_least_needed,
+)
+
+def create_loc_factory(loc_type=None, num_seating=None, space=None):
+    def create_loc(
+            loc_type=loc_type,
+            num_seating=num_seating,
+            space=space
+    ):
+        table = Location.objects.create(
+            loc_type=loc_type,
+            num_seating=num_seating,
+            space=space
+        )
+        return table
+    return create_loc
+
+def signin_user(test_obj, username='typical_user'):
+    user = User.objects.create(username=username, password='open')
     test_obj.client.force_login(user, backend=None)
 
 # Create your tests here.
-class ReservationModelTests(TestCase):
-    pass
+class ReservationModelTests(TransactionTestCase):
+    '''
+    Might want to move the concurrency tests to there own class later.
+    Inheriting from `TransactionTestCase` instead of `TestCase` seems to cause
+    a classes tests to run significantly more slowly.
+    '''
+    def test_hold_seats_for_confirmation(self):
+        '''
+        Trying to reserve seats returned by `select_least_needed` should
+        result, excepting concurrency issues, in a reservation being created,
+        and its `id` being returned.
+        '''
+        # create user
+        user1 = User.objects.create_user(
+            'Testy', 'testy@test.com', 'testypasswd'
+        )
+        user1.first_name = "Testy"
+        user1.last_name = "Smith"
+        user1.save()
 
+        # create mock request (the relevant part)
+        class request:
+            user = user1
+        self.request = request
 
+        # create fake data
+        fdg.drop_make_spaces()
+
+        seat = Location.objects.create(
+            loc_type='Patio',
+            num_seating=2,
+            space=Space.objects.all().first()
+        )
+        seat = (seat.id, None, None, None)  # only care about id
+        res_datetime = timezone.now() + datetime.timedelta(days=1)
+        start = str(res_datetime).split('+')[0]
+
+        res_id = hold_seats_for_confirmation(self.request, [seat], start)
+        reservations = Reservation.objects.all()
+
+        self.assertIsNotNone(res_id)
+        self.assertEqual(res_id, reservations.first().id)
+        self.assertEqual(reservations.first().confirmed, 0)
+
+    def test_hold_seats_for_confirmation_concurrent_block(self):
+        '''
+        If two database connections request a reservation on an intersecting
+        set of locations, the leading request should succeed, and the lagging
+        request should receive an `IntegrityError`. This function ensures that
+        the lagging request will fail with the above exception by simulating
+        the effects of a race condition. It does not rely on actual
+        concurrency and therefore performs no test of the database locks.
+        '''
+        user1 = User.objects.create_user(
+            'Testy', 'testy@test.com', 'testypasswd'
+        )
+        user1.first_name = 'Testy'
+        user1.last_name = 'Smith'
+        user1.save()
+
+        # create mock requests
+        class request:
+            user = user1
+        self.request = request
+
+        # create fake data
+        fdg.drop_make_spaces()
+        seat1 = Location.objects.create(
+            loc_type='Patio',
+            num_seating=2,
+            space=Space.objects.all().first()
+        )
+        seat2 = Location.objects.create(
+            loc_type='Patio',
+            num_seating=2,
+            space=Space.objects.all().first()
+        )
+        seats = [(seat1.id, None, None, None), (seat2.id, None, None, None)]
+
+        # "reserve" `seat2` (it won't appear in `hold_seats_...`'s query)
+        seat2.delete()
+
+        res_datetime = timezone.now() + datetime.timedelta(days=1)
+        start = str(res_datetime).split('+')[0]
+
+        with self.assertRaises(IntegrityError) as context:
+            res_id = hold_seats_for_confirmation(self.request, seats, start)
+        
+        self.assertTrue('Seats no longer available.' in str(context.exception))
+
+    def test_select_least_needed_not_enough_tables(self):
+        '''
+        Ensure that `select_least_needed` raises EOFError if no set of tables
+        is found such that 
+
+            set_capacity <= `num_tables_requested` <= set_capacity + 1.
+
+        with an appropriate message.
+        '''
+        _user = User.objects.create_user(
+            'Testy', 'testy@test.com', 'testypasswd'
+        )
+        _user.first_name = 'Testy'
+        _user.last_name = 'Smith'
+
+        class request:
+            user = _user
+        num_seats_requested = 1
+        start = timezone.now() + datetime.timedelta(hours=1)
+
+        with self.assertRaises(EOFError) as context:
+            seats, res_id = select_least_needed(
+                request,
+                num_seats_requested,
+                start
+            )
+
+        self.assertTrue(
+            'No suitable table arrangement found.' in str(context.exception)
+        )
+
+    def test_select_least_needed_tables_available_no_concurrency(self):
+        '''
+        Attempting to create a reservation while there are suitable 
+        accomodations should result in the creation of a temporary reservation
+        whose id `res_id` is returned, excepting concurrency issues.
+        '''
+        _user = User.objects.create_user(
+            'Testy',
+            't@test.com'
+            'testyboy5'
+        )
+        _user.first_name = 'Testy'
+        _user.last_name = 'Smith'
+        _user.save()
+
+        fdg.drop_make_spaces()
+        seats_standing1 = Location.objects.create(
+            loc_type='Standing',
+            num_seating=2,
+            space=Space.objects.all().first()
+        )
+        seats_standing2 = Location.objects.create(
+            loc_type='Standing',
+            num_seating=4,
+            space=Space.objects.all().first()
+        )
+
+        class request:
+            user = _user
+        start = timezone.now() + datetime.timedelta(hours=1)
+
+        for k in range(1, 7):
+            num_seats_requested = k
+
+            seats, res_id = select_least_needed(
+                request,
+                num_seats_requested,
+                start
+            )
+            reservation_id = Reservation.objects.all().first()
+            self.assertIsNotNone(reservation_id)
+            self.assertEqual(res_id, reservation_id.id)
+
+            Reservation.objects.get(pk=res_id).delete()
+
+    
+    def test_select_least_needed_tables_available_concurrency_block(self):
+        '''
+        If two requests for reservations at the same time on the same tables
+        occur simultaneously, the lagging request should resume searching at 
+        the beginning of the current space and table type. Any changes to the
+        database instigated by the lagging request should be rolled back.
+        '''
+        _user = User.objects.create_user(
+            'Testy',
+            't@test.com',
+            'testypass'
+        )
+        _user.first_name = 'Testy'
+        _user.last_name = 'Smith'
+        _user.save()
+
+        class request:
+            user = _user
+        num_seats_requested = 9
+        start = timezone.now() + datetime.timedelta(days=1)
+
+        fdg.drop_make_spaces()
+        patio = Space.objects.filter(space_name='Patio').first()
+        bar = Space.objects.filter(space_name='Bar').first()
+
+        create_patio_standing = create_loc_factory(
+            loc_type='Standing',
+            space=patio
+        )
+        table_4_seater = create_patio_standing(num_seating=4)
+        table_6_seater = create_patio_standing(num_seating=6)
+        table_2_seater_1 = create_patio_standing(num_seating=2)
+        table_2_seater_2 = create_patio_standing(num_seating=2)
+
+        tables, res_id = select_least_needed(
+            request,
+            num_seats_requested,
+            start
+        )
+
+        ID = 0
+        self.assertEqual(tables[0][ID], table_6_seater.id)
+        self.assertEqual(tables[1][ID], table_4_seater.id)
+
+        Reservation.objects.all().delete()
+
+        func = Location.objects.get(pk=table_4_seater.id).delete
+        mock_capture = 'reservation.views.TEST_SIM_RACE_CONDITION'
+        with mock.patch(mock_capture, side_effect=func):
+            tables, res_id = select_least_needed(
+                request,
+                num_seats_requested,
+                start
+            )
+
+        self.assertEqual(len(tables), 3)
+        self.assertEqual(tables[0][ID], table_6_seater.id)
+        self.assertEqual(tables[1][ID], table_2_seater_1.id)
+        self.assertEqual(tables[2][ID], table_2_seater_2.id)
+
+        
 class ReservationIndexViewTests(TestCase):
     def test_select_past_reservation(self):
         '''
@@ -48,14 +294,14 @@ class ReservationIndexViewTests(TestCase):
             ).strftime('%Y-%m-%dT%H:%M')
         })
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, escape("We don't have tables available"))
+        self.assertContains(response, escape("Sorry. We're all booked!"))
 
     def test_select_reservation_open_tables(self):
         '''
         Attempting to make a reservation for a time when tables are available
         should succeed.
         '''
-        fake_data_generator.create_data(num_res=0)
+        fdg.create_data(num_res=0)
         signin_user(self)
         response = self.client.post(reverse('reservation:request'), {
             'num_guests': 1,
@@ -63,7 +309,7 @@ class ReservationIndexViewTests(TestCase):
                 datetime.datetime.now() + datetime.timedelta(days=10)
             ).strftime('%Y-%m-%dT%H:%M')
         })
-        pprint.pprint(response.getvalue().decode())
+        # pprint.pprint(response.getvalue().decode())
         self.assertEqual(response.status_code, 200)
         self.assertContains(
             response, escape('Would you like us to reserve these seats?')
--- reservation/views.py
+++ reservation/views.py
@@ -10,23 +10,89 @@ from django.template import loader
 from django.http import HttpResponse
 
 import django.db as db
+from django.db import IntegrityError
 from django.db import transaction, connection
 from timeline.models import Reservation
 
+from reservation.test_utils import TEST_SIM_RACE_CONDITION
+
 SQL_PATH = 'reservation/sql'
 
-def select_least_needed(num_seats_requested, start, end):
+
+def hold_seats_for_confirmation(request, seats, start):
     '''
+    request: normal request object
+    seats: list of seats (`id`, `space_type`, `loc_type`, `num_seating`)
+    start: datetime lacking timezone postfix
+
+    returns >> reservation_id
+
+    Attempt to create a reservation for the seats in `seats`. If any of these
+    seats cannot be reserved, throw `IntegrityError` and cancel the
+    reservation.
+    '''
+    transaction_fragment_path = os.path.join(
+        SQL_PATH, 'set_reservation_fragment.sql'
+    )
+
+    transaction_fragment = open(transaction_fragment_path, 'r').readlines()
+    transaction_head = transaction_fragment[:2]
+    transaction_tail = transaction_fragment[2:]
+
+    res_datetime = str(start)
+    num_menus = len(seats)
+
+    for seat in seats:
+        loc_id, *_ = seat
+        transaction_tail.append(
+            f'INSERT INTO timeline_transaction (location_id, reservation_id)'
+            f' VALUES ({loc_id}, @reservation_id);'
+        )
+
+    params = [
+        request.user.id,
+        request.user.get_full_name(),
+        num_menus,
+        res_datetime.split('+')[0],
+    ]
+
+    with connection.cursor() as cursor:
+        set_vars, lock_seats = transaction_head
+        cursor.execute(set_vars, params)
+        cursor.execute(lock_seats.format(', '.join([str(s[0]) for s in seats])))
+        seats_from_query = cursor.fetchall()
+
+        if len(seats_from_query) < num_menus:
+            class ConcurrencyError(IntegrityError):
+                tables_already_reserved = set(seats) - set(seats_from_query)
+            raise ConcurrencyError('Seats no longer available.')
+        for query in transaction_tail:
+            cursor.execute(query)
+
+        cursor.execute('SELECT @reservation_id')
+        return cursor.fetchone()[0]
+
+
+def select_least_needed(request, num_seats_requested, start):
+    '''
+    request: normal request object
+    num_seats_requested: party size
+    start: reservation datetime
+
+    returns >> seats (tuple), res_id (int)
+
     Get the smallest set of available tables of the same type and location
     that fullfills the reservation but exceeds in capacity the party size by
-    no more than one seat. If no such arrangement is found, throw an
+    no more than one seat. If the reservation can't be fulfilled, throw an
     EOFError.
     '''
     size_of_table_set = 0
+    tables_remaining_after_race = None
     sql_fname = 'select_least_needed_from_available.sql'
 
     select_least_needed_query = open(os.path.join(SQL_PATH, sql_fname)).read()
 
+    end = start + datetime.timedelta(hours=1)
     with connection.cursor() as cursor:
         cursor.execute('SET @start = %s, @end = %s', [start, end])
         cursor.execute(select_least_needed_query)
@@ -37,28 +103,62 @@ def select_least_needed(num_seats_requested, start, end):
         sql_results = cursor.fetchall()
         pprint.pprint(sql_results)
 
-        for row in sql_results:
+        n = len(sql_results)
+        pos = 0
+        while pos < n:
+            row = sql_results[pos]
             loc_id, space, loc_type, num_seats = row
+
             if current_space != space or current_loc_type != loc_type:
+                section_start = pos
                 size_of_table_set = 0
                 current_space = space
                 current_loc_type = loc_type
                 result_set = []
+                tables_not_available_after_race = None
 
-            if (size_of_table_set < num_seats_requested
+            if ((size_of_table_set < num_seats_requested
+                 and
+                 size_of_table_set + num_seats <= num_seats_requested + 1)
                 and
-                size_of_table_set + num_seats <= num_seats_requested + 1):
+                (tables_not_available_after_race is None
+                 or
+                 sql_results[pos] not in tables_not_available_after_race)):
                 size_of_table_set += num_seats
                 result_set.append(row)
             if (num_seats_requested
                 <= size_of_table_set
                 <= num_seats_requested + 1):
-                break
+
+                try:
+                    TEST_SIM_RACE_CONDITION()
+                except:
+                    pass
+
+                try:
+                    with transaction.atomic():
+                        res_id = hold_seats_for_confirmation(
+                            request,
+                            result_set,
+                            start
+                        )
+                except IntegrityError as e:
+                    tables_not_available_after_race = e.tables_already_reserved
+
+                    res = (str(r[0]) for r in tables_not_available_after_race)
+                    print(f"({', '.join(res)}) reserved by another customer")
+
+                    pos = section_start - 1
+                    size_of_table_set = 0
+                    result_set = []
+                else:
+                    break
+            pos += 1
 
         if num_seats_requested <= size_of_table_set <= num_seats_requested + 1:
-            return result_set
+            return result_set, res_id
         else:
-            raise EOFError("No suitable table arrangement found anywhere.")
+            raise EOFError("No suitable table arrangement found.")
 
 
 # Create your views here.
@@ -82,68 +182,34 @@ def request(request):
         return render(request, 'reservation/index.html', {
             'error_message': "You didn't select a choice.",
         })
-    else:
-        end = start + datetime.timedelta(minutes=90)
 
     if start < timezone.now():
         return render(request, 'reservation/index.html', {
             'error_message': "Please select future accommodations."
         })
     try:
-        seats = select_least_needed(num_seats_requested, start, end)
+        seats, res_id = select_least_needed(
+            request, num_seats_requested, start
+        )
     except EOFError as e:
         print(e)
         return render(request, 'reservation/index.html', {
             'could_not_complete': (
-                "Sorry. We don't have tables available at the requested time "
-                "for a party of this size. Please select a different "
-                " reservation. Thank you!")})
+                "Sorry. We're all booked! Please try another reservation.")})
 
     request.session['reservation'] = {
+        'res_id': res_id,
         'seats': seats,
         'datetime': str(start),
         'num_menus': num_seats_requested
     }
-
+    
     template = loader.get_template('reservation/request.html')
     return HttpResponse(template.render({'seats': seats}, request))
 
-@transaction.atomic
+
 def preconfirmation(request):
-    transaction_fragment_path = os.path.join(
-        SQL_PATH,
-        'set_reservation_fragment.sql'
-    )
-    transaction_fragment = open(transaction_fragment_path, 'r').readlines()
-    transaction_head = transaction_fragment[0]
-    transaction_tail = transaction_fragment[1:]
-
-    reservation = request.session['reservation']
-
-    seats = reservation['seats']
-    res_datetime = reservation['datetime']
-    num_menus = reservation['num_menus']
-
-    for seat in seats:
-        loc_id, *_ = seat
-        transaction_tail.append((
-            f' INSERT INTO timeline_transaction (location_id, reservation_id)'
-            f' VALUES ({loc_id}, @reservation_id);'
-        ))
-
-    params = [
-        request.user.id,
-        request.user.get_full_name(),
-        num_menus,
-        res_datetime.split('+')[0],
-    ]
-
-    with connection.cursor() as cursor:
-        cursor.execute(transaction_head, params)
-        for query in transaction_tail:
-            print(query)
-            cursor.execute(query)
-
+    
     return HttpResponse('OK')
 
 def confirmation(request):
