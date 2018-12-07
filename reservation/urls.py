6970ed8c0d063fa5530d3c3b766ee9b83712ca49
King Crawford <crawboy@me.com>
2018-12-07 11:48:56 -0500

    add url paths

--- reservation/urls.py
+++ reservation/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('request/', views.request, name='request'),
     path('preconfirmation/', views.preconfirmation, name='preconfirmation'),
     path('confirmation/', views.confirmation, name='confirmation'),
+    path('reservations/', views.reservations, name='reservations'),
+    path('update/', views.update_reservation, name='update'),
+    path('cancel/', views.cancel_reservation, name='cancel'),
 ]
