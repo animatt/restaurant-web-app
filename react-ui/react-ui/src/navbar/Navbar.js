59461ea9e2a10d62f3387a9738cd6a04cbe6397e
King Crawford <crawboy@me.com>
2019-02-07 12:28:24 -0500

    Navbar component

--- react-ui/react-ui/src/navbar/Navbar.js
+++ react-ui/react-ui/src/navbar/Navbar.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class Navbar extends Component {
 	constructor(props) {
 		super(props)
 	}
 	render() {
+		if (loggedIn) {
+			const loginButton = <Link to="/login">Log in</Link>;
+		} else {
+			const loginButton = <Link to="/logout">Log Out</Link>;
+		}
 		return (
 			<div className="col-sm-2">
 			  <nav className="Navbar">
 				<ul class="sidebar-nav">
-				  <li>Home</li>
+				  <li><Link to="/">Home</Link></li>
 				  <li>Restaurants</li>
 				  <li>Reservations</li>
-				  <li>Log out</li>
-				  <li>Log in</li>
+				  <li>{loginButton}</li>
+				  <li></li>
 				</ul>
 			  </nav>
 			</div>
