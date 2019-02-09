edce19250e6e5fb68af902b244ad1a83ef9a48c4
King Crawford <crawboy@me.com>
2019-02-09 14:45:55 -0500

    add dummy login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('', views.check_credentials, name="check_crendentials"),
+    path('login', views.login, name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+
+import json
 
 # Create your views here.
 def check_credentials(request):
-    breakpoint()
-    return HttpResponse('Hello World')
+    if request.method != 'GET':
+        return HttpResponse('No')
+    
+    return HttpResponse(json.dumps({
+        'loggedIn': request.user.is_authenticated,
+        'userName': request.user.username
+    }))
+
+def login(request):
+    if request.method != 'POST':
+        return HttpResponse('No')
+    body = json.loads(request.body)
+    try:
+        username = body['username']
+        passwd = body['passwd']
+    except KeyError:
+        return HttpResponse('Incorrect params')
+
+    user = request.user if request.user.is_authenticated else (
+        authenticate(username=username, password=passwd)
+    )
+    if not user:
+        return HttpResponse('The username and password are not correct')
+    return HttpResponse('The username and password are correct')
+    # if user is not None:
